21ea093afc8b131fdf8c5466e4d5b301a0b0aa24
ronaldo suarez <ronaldo3055@gmail.com>
2022-07-25 16:58:43 -0500

    added intro, name, and icons

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,12 +1,22 @@
+import { FaGithub, FaLinkedinIn, FaInstagram } from "react-icons/fa";
+
 const Landing = () => {
-    return (
-        <>
-        <h1 className="name">Ronaldo Suarez - Software Engineer</h1>
-        <br></br>
-        <span>Hey 👋 I’m Ronaldo, a developer focused on solving problems with digital solutions.</span>
-        
-        </>
-    )
-}
+  const iconSize = 18;
+  return (
+    <>
+      <span className="name">Ronaldo Suarez - Software Engineer</span>
+      <br></br>
+      <span className="introduction">
+        Hey 👋 I’m Ronaldo, a developer focused on solving problems with digital
+        solutions.
+      </span>
+      <br></br>
+      <span className="socials align-center">
+        <FaGithub size={iconSize} /> <FaLinkedinIn size={iconSize} />{" "}
+        <FaInstagram size={iconSize} />
+      </span>
+    </>
+  );
+};
 
 export default Landing;
