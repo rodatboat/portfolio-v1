c4e56fcb56cececd35b922ccd99844e86228dbf4
ronaldo suarez <ronaldo3055@gmail.com>
2022-07-25 16:25:43 -0500

    init commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import Landing from './components/Landing';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="container">
+      <div className='row align-items-center'>
+        <div className='col-2 text-center'>{"<"}</div>
+        <div className='col content'>
+          <Landing />
+        </div>
+        <div className='col-2 text-center'>{">"}</div>
+      </div>
     </div>
+    </>
   );
 }
 
---
+++ src/components/Landing.js
@@ -0,0 +1,12 @@
+const Landing = () => {
+    return (
+        <>
+        <h1 className="name">Ronaldo Suarez - Software Engineer</h1>
+        <br></br>
+        <span>Hey 👋 I’m Ronaldo, a developer focused on solving problems with digital solutions.</span>
+        
+        </>
+    )
+}
+
+export default Landing;
