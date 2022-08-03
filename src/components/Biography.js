d5cc363dd20557e271b5ee6030883a272bfc6ff2
Ronaldo Suarez Surface <ronaldo3055@gmail.com>
2022-08-03 12:03:59 -0500

    more updates to biography component

--- src/components/Biography.js
+++ src/components/Biography.js
@@ -14,13 +14,13 @@ const Biography = () => {
         <>
         <div className="biography-component">
         <div className="biography-wrapper">
-            <img src={biography ? biography.biography.img_src    : false} />
+            <img src={biography ? biography.biography.img_src : '#'} />
             <div className="biography-content">
                 <div className="biography-intro">
-                    <span>{biography ? biography.biography.intro : false}</span>
+                    <span>{biography ? biography.biography.intro.map((c, i) => i === 1 ? (<span key={i} className='intro-company'>{c}</span>) : c) : false}</span>
                 </div>
                 <div className="biography-bio">
-                    {biography ? biography.biography.bio.map((b, i) => <span className='bio-paragraph'>{b}</span>) : false}
+                    {biography ? biography.biography.bio.map((b, i) => <span key={i} className='bio-paragraph'>{b}</span>) : false}
                 </div>
             </div>
         </div>
